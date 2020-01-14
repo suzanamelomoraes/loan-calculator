6cf3177fc19d3c2f4c6c26faf8a19300f019177e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-14 15:04:01 +1300

    start to create a function to calculate the results and define the variables to grab the necessary information from html

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 document
   .getElementById("loan-form")
   .addEventListener("submit", calculateResults);
+
+function calculateResults(e) {
+  const amountUI = document.getElementById("amount");
+  const interestUI = document.getElementById("interest");
+  const yearsUI = document.getElementById("years");
+  const monthlyPaymentUI = document.getElementById("monthly-payment");
+  const totalPaymentUI = document.getElementById("total-payment");
+  const totalInterestUI = document.getElementById("total-interest");
+
+  const initialAmount = parseFloat(amountUI.value);
+  const calculatedInterested = interestUI.value / 100 / 12;
+  const calculatedPayments = parseFloat(yearsUI.value) * 12;
+
+  e.preventDefault();
+}
