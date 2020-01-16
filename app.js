99c7c7d060d81e5a5e58c81792e25751b872e20d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-17 12:26:06 +1300

    refactor const variables declaration

--- app.js
+++ app.js
@@ -9,19 +9,17 @@ document.getElementById("loan-form").addEventListener("submit", function(e) {
 });
 
 function calculateResults() {
-  const amountUI = document.getElementById("amount");
-  const interestUI = document.getElementById("interest");
-  const yearsUI = document.getElementById("years");
-  const monthlyPaymentUI = document.getElementById("monthly-payment");
-  const totalPaymentUI = document.getElementById("total-payment");
-  const totalInterestUI = document.getElementById("total-interest");
-
-  const initialAmount = parseFloat(amountUI.value);
-  const calculatedInterest = interestUI.value / 100 / 12;
-  const calculatedPayments = parseFloat(yearsUI.value) * 12;
-
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (initialAmount * x * calculatedInterest) / (x - 1);
+  const amountUI = document.getElementById("amount"),
+    interestUI = document.getElementById("interest"),
+    yearsUI = document.getElementById("years"),
+    monthlyPaymentUI = document.getElementById("monthly-payment"),
+    totalPaymentUI = document.getElementById("total-payment"),
+    totalInterestUI = document.getElementById("total-interest"),
+    initialAmount = parseFloat(amountUI.value),
+    calculatedInterest = interestUI.value / 100 / 12,
+    calculatedPayments = parseFloat(yearsUI.value) * 12,
+    x = Math.pow(1 + calculatedInterest, calculatedPayments),
+    monthly = (initialAmount * x * calculatedInterest) / (x - 1);
 
   if (isFinite(monthly)) {
     monthlyPaymentUI.value = monthly.toFixed(2);
@@ -44,10 +42,9 @@ function showError(error) {
 
   document.getElementById("loading").style.display = "none";
 
-  const errorDiv = document.createElement("div");
-
-  const card = document.querySelector(".card");
-  const heading = document.querySelector(".heading");
+  const errorDiv = document.createElement("div"),
+    card = document.querySelector(".card"),
+    heading = document.querySelector(".heading");
 
   errorDiv.className = "alert alert-danger";
 
